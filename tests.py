fb00c9dab8388ec029c195a6d5879d10f6dc9d1f
olkabul <47484854+olkabul@users.noreply.github.com>
2021-08-19 11:15:40 +0300

    adding

--- tests.py
+++ tests.py
@@ -12,18 +12,21 @@ class MyTestCase(unittest.TestCase):
         fake_args = FakeArgs()
         fake_args.machine = True
         real_result = get_result_by_argument(fake_args)
-        assert machine_formatting_expected == real_result
+        assert machine_formatting_expected == real_result, \
+            f"Expected result: {machine_formatting_expected}, actual: {real_result}"
 
     def test_circumflex_format(self):
         fake_args = FakeArgs()
         fake_args.under = True
         real_result = get_result_by_argument(fake_args)
-        assert under_formatting_expected == real_result
+        assert under_formatting_expected == real_result, \
+            f"Expected result: {under_formatting_expected}, actual: {real_result}"
 
     def test_basic_format(self):
         fake_args = FakeArgs()
         real_result = get_result_by_argument(fake_args)
-        assert basic_formatting_expected == real_result
+        assert basic_formatting_expected == real_result, \
+            f"Expected result: {basic_formatting_expected}, actual: {real_result}"
 
 
 if __name__ == '__main__':
